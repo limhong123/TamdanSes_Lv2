7304939d04231ed75f3b1c691ea7c065777feff3
limhong123 <limhong592@gmail.com>
2026-07-17 00:34:08 +0700

    Fix firebase config

--- backend/app/core/firebase_config.py
+++ backend/app/core/firebase_config.py
@@ -7,16 +7,19 @@ from firebase_admin import credentials
 
 if not firebase_admin._apps:
 
-    firebase_json = os.getenv("FIREBASE_SERVICE_ACCOUNT")
+    firebase_value = os.getenv("FIREBASE_SERVICE_ACCOUNT")
 
-    if firebase_json:
-        # Render
-        cred = credentials.Certificate(json.loads(firebase_json))
+    if firebase_value:
+        firebase_value = firebase_value.strip()
+
+        if firebase_value.startswith("{"):
+            cred = credentials.Certificate(json.loads(firebase_value))
+        else:
+            cred = credentials.Certificate(firebase_value)
+
     else:
-        # Local
         BASE_DIR = Path(__file__).resolve().parents[2]
         SERVICE_ACCOUNT = BASE_DIR / "firebase_service_account.json"
-
         cred = credentials.Certificate(str(SERVICE_ACCOUNT))
 
     firebase_admin.initialize_app(cred)
